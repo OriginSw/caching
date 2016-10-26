cdc6b67aee5cbb3497113398941b48c3aff4a8f8
bardock <fedesanchez89@gmail.com>
2016-10-26 15:08:48 -0300

    Sixeyed.Caching v.1.0.1

--- Sixeyed.Caching/Properties/AssemblyInfo.cs
+++ Sixeyed.Caching/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@
 [assembly: AssemblyCompany("bardock")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
